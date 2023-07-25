fc4335339bb1affc694a4e3fb1c3012165f7e844
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-07-26 01:02:23 +0530

    Initial Commit

---
+++ ToDoList-App/ToDoList_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_AppApp.swift
+//  ToDoList-App
+//
+//  Created by Vithushan Logan on 2023-07-26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
